6e6c6101dc79d9ae24c53b17c7936fe2e3c1aaeb
jjun-maeng <jun777k@naver.com>
2023-11-13 16:31:44 +0900

    갤러리 추가

--- src/views/classsample.vue
+++ src/views/classsample.vue
@@ -1,5 +1,43 @@
 <template>
-    <subpage :pagetit="'갤러리'"></subpage>
+    <subpage :pagetit="'갤러리'">
+    <div class="gallery">
+        <img src="images/lay01.jpg"/>
+        <img src="images/lay02.jpg"/>
+        <img src="images/lay03.jpg"/>
+        <img src="images/lay011.jpg"/>
+        <img src="images/hue01.jpg"/>
+        <img src="images/hue02.jpg"/>
+        <img src="images/hue05.jpg"/>
+        <img src="images/bcase02.jpg"/>
+        <img src="images/bcase03.jpg"/>
+        <img src="images/frp01.jpg"/>
+        <img src="images/frp06.jpg"/>
+        <img src="images/clone01.jpg"/>
+        <img src="images/clone03.png"/>
+        <img src="images/clone04.png"/>
+        <img src="images/clone05.jpg"/>
+        <img src="images/huerest11.jpg"/>
+        <img src="images/sprout01.jpg"/>
+        <img src="images/sprout10.jpg"/>
+        <img src="images/ref07.jpg"/>
+        <img src="images/skt01.jpg"/>
+        <img src="images/retro01.jpg"/>
+        <img src="images/tang05.jpg"/>
+        <img src="images/tang06.jpg"/>
+        <img src="images/dory04.jpg"/>
+        <img src="images/three01.jpg"/>
+        <img src="images/room01.jpg"/>
+        <img src="images/room02.jpg"/>
+        <img src="images/room03.jpg"/>
+        <img src="images/room04.jpg"/>
+        <img src="images/room05.jpg"/>
+        <img src="images/room06.jpg"/>
+        <img src="images/room07.jpg"/>
+        <img src="images/room08.jpg"/>
+        <img src="images/room09.jpg"/>
+        <img src="images/room10.jpg"/><br>
+    </div>
+    </subpage>
 </template>
 <script>
 import Subpage from "@/layout/components/Subpage.vue";
